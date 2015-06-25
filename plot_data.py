d4d5466662ce39ff67b6ce9f7e5f3abd0d0460fb
heysep <sdvakili@gmail.com>
2015-06-25 13:49:32 -0400

    plot_data.py now handles multiple files

--- plot_data.py
+++ plot_data.py
@@ -1,32 +1,42 @@
 import numpy as np
 import matplotlib.pyplot as plt
+import glob
 
-# load the first data file
-data = np.loadtxt(fname='inflammation-01.csv', delimiter=',')
+# get a list of files that match the pattern 'inflammation-*.csv'
+filenames = glob.glob('inflammation-*.csv')
+filenames = filenames[0:3]
 
-# create a matplotlib figure with a certain size
-fig = plt.figure(figsize=(10.0, 3.0))
+# run it for each file
+for filename in filenames:
+	
+	print filename
 
-# create my figure axes
-axes1 = fig.add_subplot(1, 3, 1)
-axes2 = fig.add_subplot(1, 3, 2)
-axes3 = fig.add_subplot(1, 3, 3)
+	# load the first data file
+	data = np.loadtxt(fname=filename, delimiter=',')
 
-# fill axes1
-axes1.set_ylabel('average')
-axes1.plot(data.mean(axis=0))
+	# create a matplotlib figure with a certain size
+	fig = plt.figure(figsize=(10.0, 3.0))
 
-# fill axes2
-axes2.set_ylabel('max')
-axes2.plot(data.max(axis=0))
+	# create my figure axes
+	axes1 = fig.add_subplot(1, 3, 1)
+	axes2 = fig.add_subplot(1, 3, 2)
+	axes3 = fig.add_subplot(1, 3, 3)
 
-# fill axes3
-axes3.set_ylabel('min')
-axes3.plot(data.min(axis=0))
+	# fill axes1
+	axes1.set_ylabel('average')
+	axes1.plot(data.mean(axis=0))
 
-# make the figure have a tight layout, no overlapping text
-fig.tight_layout()
+	# fill axes2
+	axes2.set_ylabel('max')
+	axes2.plot(data.max(axis=0))
 
-# show the figure
-plt.show(fig)
+	# fill axes3
+	axes3.set_ylabel('min')
+	axes3.plot(data.min(axis=0))
+
+	# make the figure have a tight layout, no overlapping text
+	fig.tight_layout()
+
+	# show the figure
+	plt.show(fig)
 
